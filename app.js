2a3b297ab776658296fa6578467efad31a45224f
LukeRekt <lucasnunu2010@hotmail.com>
2018-06-21 00:13:13 -0300

    update

--- app.js
+++ app.js
@@ -1794,6 +1794,7 @@ bot.on("message", async message => {
     let mUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     if(!mUser) return message.channel.send("Não achei o fiato!");
 
+      message.channel.send(mUser);
       message.channel.send("${mUser}", {files: ["https://media.giphy.com/media/iWEIxgPiAq58c/giphy.gif"]});
     return;
   }
