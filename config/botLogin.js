c99b053aef4145e3f052ab7acb8fa9a676c2bee9
LukeRekt <lucasnunu2010@hotmail.com>
2018-11-11 15:53:50 -0200

    update

--- config/botLogin.js
+++ config/botLogin.js
@@ -1,5 +1,5 @@
 module.exports = {
-	token: "Token do luixo",
+	token: "NDM3NzUyNDg0OTYzMDI0OTA3.Dsn-HA.JTKQq8LNsayTMD6ogfPjPk7YwG8",
 	youtubeAPI: "AIzaSyAwsiSG1SVIGUL_-UF9rw0v2gGXMsMnaJ0",
 	owner_id: "223207253522644992"
 }
