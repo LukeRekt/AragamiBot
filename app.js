9603dfe70a52dbe88725101632a9990fd2a20653
LukeRekt <lucasnunu2010@hotmail.com>
2019-01-15 15:16:41 -0200

    update

--- app.js
+++ app.js
@@ -1822,8 +1822,8 @@ bot.on("message", async message => {
     if(cmd === `${initcmd}privado`){
     let bUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     if(!bUser) return message.channel.send("Não achei o fiato!");
-	if(!message.member.hasPermission("MUTE_MEMBERS")) return message.channel.send("Sem permissão fiato! >:C");
       message.guild.member(bUser).sendMessage(bReason);
+	  message.delete();
     return;
   }
 
