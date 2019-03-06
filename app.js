3bf148cecfc72f7d4f46ddae1aff45523d86626f
LukeRekt <lucasnunu2010@hotmail.com>
2019-03-06 00:01:42 -0300

    update

--- app.js
+++ app.js
@@ -1766,7 +1766,7 @@ bot.on("message", async message => {
 		if(!member) return message.reply("Você precisa mencionar alguem");
 		let rankmute = message.guild.roles.find("name", "Mutado");
 		if(!rankmute) return message.reply("não existe um cargo com nome de Mutado");
-		if(!message.member.hasPermission("MUTE_MEMBERS")) return message.channel.send("Sem permissão fiato! >:C");
+		if(!message.member.hasPermission("MUTE_MEMBERS")) || isOwner(message) return message.channel.send("Sem permissão fiato! >:C");
 		let params = message.content.split(" ").slice("1");
 		let time = params[1];
 		if(!time) return message.reply('Coloca um tempo fiato >:C');
