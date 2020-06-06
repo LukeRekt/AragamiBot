b5c79fe8155339964610211d2cd10bff1f2215f2
LukeRekt <47284018+LukeRekt@users.noreply.github.com>
2020-06-06 11:40:55 -0300

    update

--- app.js
+++ app.js
@@ -1752,28 +1752,28 @@ bot.on('guildMemberAdd', member => {
 
 });
 
-bot.on("guildMemberAdd", async member => {
+//bot.on("guildMemberAdd", async member => {
 
-  let canal = bot.channels.get("445793368078024706")
-  let fonte = await jimp.loadFont(jimp.FONT_SANS_32_BLACK)
-  let mask = await jimp.read('mascara.png')
-  let fundo = await jimp.read('fundo.png')
+  //let canal = bot.channels.get("445793368078024706")
+  //let fonte = await jimp.loadFont(jimp.FONT_SANS_32_BLACK)
+  //let mask = await jimp.read('mascara.png')
+  //let fundo = await jimp.read('fundo.png')
 
-  jimp.read(member.user.displayAvatarURL).then(avatar => {
-  avatar.resize(130, 130)
-  mask.resize(130, 130)
-  avatar.mask(mask)
+  //jimp.read(member.user.displayAvatarURL).then(avatar => {
+  //avatar.resize(130, 130)
+  //mask.resize(130, 130)
+  //avatar.mask(mask)
 
-  fundo.print(fonte, 170, 175, member.user.username)
-  fundo.composite(avatar, 40, 90).write('bemvindo.png')
-  canal.send(``, { files: ["bemvindo.png"] })
+  //fundo.print(fonte, 170, 175, member.user.username)
+  //fundo.composite(avatar, 40, 90).write('bemvindo.png')
+  //canal.send(``, { files: ["bemvindo.png"] })
 
-  console.log('Imagem enviada para o Discord')
-  })
-  .catch(err => {
-  console.log('error avatar')
-  })
-})
+  //console.log('Imagem enviada para o Discord')
+  //})
+  //.catch(err => {
+  //console.log('error avatar')
+  //})
+//})
 
 //Comando de mutar por tempo
 
