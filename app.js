3eed80f55f02af56f83cb50b654f4ee2ac31daaa
LukeRekt <47284018+LukeRekt@users.noreply.github.com>
2020-06-07 18:33:55 -0300

    update

--- app.js
+++ app.js
@@ -18,13 +18,31 @@ const logsPath = path.join(__dirname, 'logs');
 const configPath = path.join(__dirname, 'config');
 const modulesPath = path.join(__dirname, 'modules');
 const prefix = '-'
+let coins = require("./coins.json")
 
 const botLogin = require(path.join(configPath, 'botLogin.js'));
 const yt = require(path.join(modulesPath, 'youtube.js'));
 const botPreferenceFile = path.join(configPath, 'preference.json');
 
 
+fs.readdir("./commands/", (err, files) => {
+	if(err) console.log(err);
 
+	let jsfile = files.filter(f => f.split(".").pop() === "js")
+	if(jsfile.length <= 0){
+		console.log("Couldnt find any commands");
+		return;
+	}
+	jsfile.forEach((f) => {
+		let props = require(`./commands/${f}`);
+		console.log(`${f} loaded!`);
+		bot.commands.set(props.help.name, props);
+
+		props.help.aliases.forEach(alias => {
+			bot.aliases.set(alias, props.help.name);
+		})
+	})
+})
 
 try{
 	var botVersion = require(path.join(__dirname, 'package.json')).version;
