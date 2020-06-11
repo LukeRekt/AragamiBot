5d2821d2eff67493d99b4e1f14b5f22a05d9e698
LukeRekt <47284018+LukeRekt@users.noreply.github.com>
2020-06-11 14:45:36 -0300

    update

--- commands/fakecoins.js
+++ commands/fakecoins.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 mongoose.connect("mongodb://179.35.0.113:27017/Tutorial", {
 	useNewUrlParser: true
 });
-const Money = require("./models/money.js")
+const Money = require("../models/money.js")
 
 module.exports.run = async(bot, message, args) => {
 
