e80e7a6da31082ed169742dd0f1da3e64727920e
LukeRekt <47284018+LukeRekt@users.noreply.github.com>
2020-06-14 15:56:11 -0300

    update

--- commands/leaders.js
+++ commands/leaders.js
@@ -13,17 +13,18 @@ module.exports.run = async(bot, message, args) => {
 Coins.find({
   serverID: message.guild.id
 }).sort([
-  ['coins', 'descending']
+  ['money', 'descending']
 ]).exec((err, res) => {
   if(err) console.log(err);
 
   let embed = new Discord.RichEmbed()
   .setTitle("Magnatas do server")
-
+//se nao tiver resultados
   if(res.length === 0){
     embed.setColor("RED");
     embed.addField("sem dados", "escreva algo no chat")
   }else if(res.length < 10){
+    //menos de 10 resultados
 embed.setColor("BLURPLE");
 for(i = 0; i < res.length; i++){
   let member = message.guild.members.get(res[i].userID) || "User saiu"
@@ -32,8 +33,7 @@ for(i = 0; i < res.length; i++){
   }else {
 embed.addField(`${i + 1}. ${member.user.username}`, `Money: ${res[i].coins}`);
   }
-
-  }
+ }
 }else {
   embed.setColor("BLURPLE");
   for(i = 0; i < 10; i++){
