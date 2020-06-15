3de546a783f1c20f555be8e3956638ad7864ec81
LukeRekt <47284018+LukeRekt@users.noreply.github.com>
2020-06-15 18:13:53 -0300

    update

--- commands/roubar.js
+++ commands/roubar.js
@@ -9,14 +9,41 @@ const Money = require("../models/money.js")
 
 module.exports.run = async (bot, message, args) => {
      let numero = Math.floor(Math.random() * 10) + 1;
-     message.reply(`caiu ${numero}`)
-     if(numero > 5){
-       message.reply(`ganhou`)
-     }else {
-       message.reply(`perdeu`)
-     }
+     let numeroroub = Math.floor(Math.random() * 100) + 1;
+     let member = message.guild.member(message.mentions.users.first())
 
-	    }
+     if(!member) return message.reply(`O usuário não foi encontrado.`)
+
+      if(args[1] == null) return message.reply("quantidade pfv");
+
+    	    Money.findOne({serverID: message.guild.id, userID: message.author.id},(err,loc) => {
+    	    Money.findOne({serverID: message.guild.id, userID: member.id},(err,data) => {
+    	        if(!data){
+    	            let errorMess = new Discord.RichEmbed()
+    	            .setColor('RED')
+    	            .setDescription(`o User **${member.user.tag}** nao esta no banco de dados.`)
+    	            return message.channel.send(errorMess)
+    	        }else{
+
+    	            if(data.money < 100) return message.reply(`Você não tem tantas moedas.`)
+    	            if(data.userID == member.id) return message.reply(`Você não pode transferir moedas para si mesmo!`)
+    	            if(member.user.bot) return message.reply(`Bots não são humanos.`)
+
+                  if(numero > 5){
+    	            let embed = new Discord.RichEmbed()
+    	            .setColor('RED')
+    	            .setDescription(`**${message.author.username}** roubou **${numeroroub}** de ${member}`)
+    	            loc.money += Math.floor(parseInt(numeroroub));
+    	            data.money -= Math.floor(parseInt(numeroroub));
+    	            loc.save(); data.save()
+    	            message.channel.send(embed)
+                }else {
+                  return message.reply("perdeu--mudar msg");
+                }
+    	                }
+    	            })
+    	        })
+    	    }
 
 module.exports.help = {
   name: "roubar",
