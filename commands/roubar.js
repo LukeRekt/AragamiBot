c26be814d0e193170708c49e00913d0e5bc48558
LukeRekt <47284018+LukeRekt@users.noreply.github.com>
2020-06-19 12:23:50 -0300

    update

--- commands/roubar.js
+++ commands/roubar.js
@@ -8,7 +8,7 @@ mongoose.connect("mongodb+srv://lucasrsl1:1010101010@cluster0-rzkwu.mongodb.net/
 const Money = require("../models/money.js")
 
 module.exports.run = async (bot, message, args) => {
-     let numero = Math.floor(Math.random() * 10) + 1;
+     let numeroroll = Math.floor(Math.random() * 10) + 1;
      let numeroroub = Math.floor(Math.random() * 100) + 1;
      let member = message.guild.member(message.mentions.users.first())
 
@@ -30,12 +30,12 @@ module.exports.run = async (bot, message, args) => {
     	           // if(data.userID == member.id) return message.reply(`Você não pode transferir moedas para si mesmo!`)
     	            if(member.user.bot) return message.reply(`Bots não são humanos.`)
 
-                  if(numero > 5){
+                  if(numeroroll > 5){
     	            let embed = new Discord.RichEmbed()
     	            .setColor('RED')
-    	            .setDescription(`**${message.author.username}** roubou **${numeroroub}** de ${member}`)
-    	            loc.money += Math.floor(parseInt(numeroroub));
-    	            data.money -= Math.floor(parseInt(numeroroub));
+    	            .setDescription(`**${message.author.username}** roubou **${numeroroll}** de ${member}`)
+    	            loc.money += Math.floor(parseInt(numeroroll));
+    	            data.money -= Math.floor(parseInt(numeroroll));
     	            loc.save(); data.save()
     	            message.channel.send(embed)
                 }else {
