8a6823705e45f382327d56604878df68fba1149b
LukeRekt <47284018+LukeRekt@users.noreply.github.com>
2020-06-19 11:59:14 -0300

    update

--- commands/roubar.js
+++ commands/roubar.js
@@ -56,7 +56,7 @@ module.exports.run = async (bot, message, args) => {
     	                })
     	            })
     	        }
-    	    }
+    	    
 
 module.exports.help = {
   name: "roubar",
