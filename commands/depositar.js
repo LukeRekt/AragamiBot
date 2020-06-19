194b0603933f65c19abe96466b02813ab7eac9e9
LukeRekt <47284018+LukeRekt@users.noreply.github.com>
2020-06-19 12:41:53 -0300

    update

--- commands/depositar.js
+++ commands/depositar.js
@@ -35,7 +35,6 @@ module.exports.run = async (bot, message, args) => {
 							loc.money -= Math.floor(parseInt(loc.money));
 
 							loc.save();
-							loc.save();
 	                }
 	        })
 	    }
