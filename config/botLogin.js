5a0ebbf75327a80c666431c9b200b790c936956b
LukeRekt <47284018+LukeRekt@users.noreply.github.com>
2020-06-30 13:03:47 -0300

    ops
    token removido

--- config/botLogin.js
+++ config/botLogin.js
@@ -1,5 +1,5 @@
 module.exports = {
-	token: "NDM3NzUyNDg0OTYzMDI0OTA3.Dsn-HA.JTKQq8LNsayTMD6ogfPjPk7YwG8",
-	youtubeAPI: "AIzaSyCRn_PmhQTn6N0beNZcwAA5abYsd7F-k3E",
+	token: "aaa",
+	youtubeAPI: "aa-k3E",
 	owner_id: "223207253522644992"
 }
