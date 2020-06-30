700ae277417b740ea038707f6a481a97538fbfd0
LukeRekt <47284018+LukeRekt@users.noreply.github.com>
2020-06-30 13:25:30 -0300

    update top final

---
+++ README.md
@@ -0,0 +1,2 @@
+# AragamiBot
+bot do aragami
--- config/botLogin.js
+++ config/botLogin.js
@@ -1,5 +1,5 @@
 module.exports = {
-	token: "",
-	youtubeAPI: "",
+	token: "removido por motivos de seguranca",
+	youtubeAPI: "removido por motivos de seguranca",
 	owner_id: "223207253522644992"
 }
