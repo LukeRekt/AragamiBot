20f22dfc0bf379ad04d7cf4e0aaac3b75c79ff50
LukeRekt <47284018+LukeRekt@users.noreply.github.com>
2020-06-30 10:44:31 -0300

    update

--- commands/roubar.js
+++ commands/roubar.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 const fs = require("fs");
 const mongoose = require("mongoose");
+const talkedRecently = new Set();
 mongoose.connect("mongodb+srv://lucasrsl1:1010101010@cluster0-rzkwu.mongodb.net/Teste?retryWrites=true&w=majority", {
 	useNewUrlParser: true
 });
@@ -11,23 +12,25 @@ module.exports.run = async (bot, message, args) => {
      let numeroroll = Math.floor(Math.random() * 10) + 1;
      let numeroroub = Math.floor(Math.random() * 100) + 1;
      let member = message.guild.member(message.mentions.users.first())
-		 const talkedRecently = new Set();
 
-		 if (talkedRecently.has(message.author.id)) {
-		             message.channel.send("Piruzinho " + msg.author);
-		     } else {
+
+
       talkedRecently.add(message.author.id);
      if(!member) return message.reply(`O usuário não foi encontrado.`)
      if(message.author.id === member.id) return message.reply("voce nao pode roubar vc mesmo");
 
     	    Money.findOne({serverID: message.guild.id, userID: message.author.id},(err,loc) => {
     	    Money.findOne({serverID: message.guild.id, userID: member.id},(err,data) => {
+
     	        if(!data){
     	            let errorMess = new Discord.RichEmbed()
     	            .setColor('RED')
     	            .setDescription(`o User **${member.user.tag}** nao esta no banco de dados.`)
     	            return message.channel.send(errorMess)
     	        }else{
+								if (talkedRecently.has(message.author.id)) {
+														message.channel.send("Piruzinho " + msg.author);
+										} else {
 
     	            if(data.money < 100) return message.reply(`a pessoa nao tem dinheiro para roubar.`)
     	           // if(data.userID == member.id) return message.reply(`Você não pode transferir moedas para si mesmo!`)
@@ -47,10 +50,16 @@ module.exports.run = async (bot, message, args) => {
 									.setDescription(`**${message.author.username}** não conseguiu roubar de ${member}`)
 									message.channel.send(embed)
                 }
+								setTimeout(() => {
+          
+          talkedRecently.delete(msg.author.id);
+        }, 60000);
+    }
+								}
     	                }
     	            })
     	        })
-							}
+
     	    }
 
 module.exports.help = {
