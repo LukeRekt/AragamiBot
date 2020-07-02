5a5c277d4455af2cabbdbb1ffb55c12de44cdfae
LukeRekt <47284018+LukeRekt@users.noreply.github.com>
2020-07-02 18:45:05 -0300

    troquei e fds

--- app.js
+++ app.js
@@ -1897,14 +1897,6 @@ bot.on("message", async message => {
 	bot.channels.get("451844584012644362").send(bReason)
     return;
   }
-  if(cmd === `${initcmd}roubo`){
-	if(roubo === true){
-		message.reply('entrou no roubo')
-	}else{
-		message.reply('nenhum roubo acontecendo')
-	}
-    return;
-  }
 
     if(cmd === `${initcmd}privado`){
     let bUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
@@ -1993,24 +1985,6 @@ money.save().catch(err => console.log(err));
 
 });
 
-//lootbox
-bot.on('ready', () => {
-	var testeCanal = bot.channels.find(channel => channel.id === '446837976597528586');
-
-	setInterval(() => {
-	//testeCanal.send("banco encontrado escreva **roubar**", {files: ["https://cdn1.iconfinder.com/data/icons/ecommerce-and-business-icon-set/256/bank.png"]});
-  roubo = true;
-  testeCanal.send(`Debug: status do roubo : ${roubo}`);
-  var myInterval = setInterval(() => {
-	roubo = false;
-	//testeCanal.send("adasdasdsadasd", {files: ["https://cdn1.iconfinder.com/data/icons/ecommerce-and-business-icon-set/256/bank.png"]});
-	testeCanal.send(`Debug: status do roubo : ${roubo}`);
-	clearInterval(myInterval);
-
-}, 10000);
-
-}, 30000)
-});
 //ping
 var http = require("http");
 setInterval(function() {
---
+++ commands/roubo.js
@@ -0,0 +1,48 @@
+const Discord = require("discord.js");
+const fs = require("fs");
+const mongoose = require("mongoose");
+const roubo = false;
+mongoose.connect(process.env.mongosenha, {
+	useNewUrlParser: true
+});
+
+const Money = require("../models/money.js")
+
+module.exports.run = async (bot, message, args) => {
+bot.on('ready', () => {
+	var testeCanal = bot.channels.find(channel => channel.id === '446837976597528586');
+
+	setInterval(() => {
+	//testeCanal.send("banco encontrado escreva **roubar**", {files: ["https://cdn1.iconfinder.com/data/icons/ecommerce-and-business-icon-set/256/bank.png"]});
+  roubo = true;
+  testeCanal.send(`Debug: status do roubo : ${roubo}`);
+  var myInterval = setInterval(() => {
+	roubo = false;
+	//testeCanal.send("adasdasdsadasd", {files: ["https://cdn1.iconfinder.com/data/icons/ecommerce-and-business-icon-set/256/bank.png"]});
+	testeCanal.send(`Debug: status do roubo : ${roubo}`);
+	clearInterval(myInterval);
+
+}, 10000);
+
+}, 30000)
+});
+
+
+	    Money.findOne({serverID: message.guild.id, userID: message.author.id},(err,loc) => {
+	    Money.findOne({serverID: message.guild.id, userID: member.id},(err,data) => {
+
+	    if(roubo === true){
+		message.reply('entrou no roubo')
+	     }else{
+		 message.reply('nenhum roubo acontecendo')
+	}
+  
+	     
+	            })
+	        })
+	    }
+
+module.exports.help = {
+  name: "roubo",
+  aliases: ["heist"]
+}
