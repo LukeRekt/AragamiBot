831bd51af51afe2c6653649a72988b0a4834242c
LukeRekt <47284018+LukeRekt@users.noreply.github.com>
2020-07-25 15:10:05 -0300

    +abracar /hug

---
+++ commands/hug.js
@@ -0,0 +1,27 @@
+
+const superagent = require("superagent");
+const Discord = require('discord.js');
+module.exports.run = async (bot, message, args) => {
+
+    let User = message.guild.members.get(args[0]) || message.mentions.members.first();
+    if (!User) return message.channel.send("Mencione alguém para da um tapa.");
+    if (User.id == message.author.id) return message.reply("você não pode dar um tapa em si mesmo.")
+
+    const {
+        body
+    } = await superagent
+        .get(`https://nekos.life/api/v2/img/hug`);
+
+    let Embed = new Discord.RichEmbed()
+        .setDescription(`<@${message.author.id}> Abraça <@${User.id}>`)
+        .setImage(body.url)
+        .setColor("RANDOM")
+        .setTimestamp()
+
+    message.channel.send(Embed)
+}
+
+module.exports.help = {
+  name: "abraçar",
+  aliases: ["hug"]
+}
