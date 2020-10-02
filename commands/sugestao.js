043da20e4014e0a599638aebf1baae7da59cc2ca
LukeRekt <47284018+LukeRekt@users.noreply.github.com>
2020-10-02 19:37:54 -0300

    comando de sugestao
    sim

---
+++ commands/sugestao.js
@@ -0,0 +1,25 @@
+
+const Discord = require('discord.js');
+module.exports.run = async (bot, message, args) => {
+    let bReason = args.join(" ").slice(22);
+
+    let banEmbed = new Discord.RichEmbed()
+    .setDescription("---Banido---")
+    .setColor("#bc0000")
+    .addField("Banido por", `<@${message.author.id}>`)
+	.addField("Hora", message.createdAt)
+    .addField("Sugestão: ", bReason);
+    let incidentchannel = message.guild.channels.find(`name`, "sugestoes");
+    if(!incidentchannel) return message.channel.send("não achei o canal ;-;.");
+
+    incidentchannel.send(banEmbed);
+
+
+    return;
+  }
+
+
+module.exports.help = {
+  name: "sugestao",
+  aliases: ["sugestion"]
+}
